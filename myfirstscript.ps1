d2ea8df8a9491a4257761ddc4805661f993be2ee
shahar blank <shahar9b@gmail.com>
2021-01-13 18:33:31 +0200

    Add first script

---
+++ myfirstscript.ps1
@@ -0,0 +1,26 @@
+﻿#prameters 
+param(
+[parameter(mandatory=$true)]
+[string]
+$url,
+[parameter(mandatory=$true)]
+[string]
+$operation
+)
+if($operation -eq "count")
+{
+  $charCount = $url.Length
+  Write-Host $charCount
+}
+Elseif($operation -eq "invert")
+{
+  $invert = $url[-1..-$url.Length] -join ''
+  Write-Host $invert
+}
+
+            
+
+
+
+
+
